1c407077b47036c8ca123d9088164923b03fbdfb
Budgerigar295 <samardude67@gmail.com>
2025-03-15 15:46:22 -0400

    feat: Preparing RobotContainer to handle inputs from the controller.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,10 +44,11 @@ public class RobotContainer {
   private LEDSubsystem lightSubsystem = LEDSubsystem.getInstance();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private static final CommandXboxController driverXbox = new CommandXboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    
     // Configure the trigger bindings
     configureBindings();
   }
@@ -64,4 +65,8 @@ public class RobotContainer {
   private void configureBindings() {
     
   }
+
+  public static CommandXboxController getDriverController() {
+    return driverXbox;
+}
 }
