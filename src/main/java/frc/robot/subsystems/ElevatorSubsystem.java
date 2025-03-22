b6352994710546b19d14635d4ae5ef9883af313c
Team5667 <46071866+Team5667@users.noreply.github.com>
2025-03-22 14:12:53 -0400

    chore: additional methods

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -14,4 +14,44 @@ public class ElevatorSubsystem implements Subsystem {
         m_controlMotor1 = new TalonFX(ElevatorConstants.CONTROL_MOTOR_1_ID);
         m_controlMotor2 = new TalonFX(ElevatorConstants.COnTROL_MOTOR_2_ID);
     }
+
+    /**
+     * This will stop all running motors. Can be used in emergencies.
+     */
+    public void stopAllMotors() {
+        m_controlMotor1.set(0); // Stop the first motor.
+        m_controlMotor2.set(0); // Stop the second motor.
+    }
+
+    /**
+     * This will get speeds for motor number 1.
+     */
+    public double getMotor1Speed() {
+        return m_controlMotor1.get();
+    }
+
+    /**
+     * This will get speeds for motor number 2.
+     */
+    public double getMotor2Speed() {
+        return m_controlMotor2.get();
+    }
+
+    /**
+     * This will set the motor speed for motor number 1.
+     */
+    public void setMotor1Speed(double speed) {
+        m_controlMotor1.set(speed);
+    }
+
+    /**
+     * This will set the motor speed for motor number 2.
+     */
+    public void setMotor2Speed(double speed) {
+        m_controlMotor2.set(speed);
+    }
+
+    public void periodic(){
+        
+    }
 }
